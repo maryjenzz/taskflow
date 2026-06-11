1e27443294df18249b239c654fa390f93eda5405
maryjenzz <mylenamari34@gmail.com>
2026-06-11 19:29:07 -0300

    railway commit

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,12 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASS:', process.env.DB_PASS ? 'definido' : 'undefined');
+
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'taskflow',
   process.env.DB_USER || 'root',
